8d14540429d217d55cb949ab6f75c5929a59021d
David <david.lin0424@gmail.com>
2021-04-10 15:47:51 +0800

    add requirements.txt

--- whichframe.py
+++ whichframe.py
@@ -7,7 +7,7 @@ import clip as openai_clip
 import torch
 import math
 import numpy as np
-import plotly.express as px
+# import plotly.express as px
 # import datetime
 import SessionState
 import tempfile
